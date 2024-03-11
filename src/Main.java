9721bcf645dedb161d3ea2632c68f17a73fa6d11
Miroslav Tsadkin <Luckymirik@mail.ru>
2024-03-11 13:49:52 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        JFrame frame = new JFrame("Простое окно");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        frame.setSize(400,300);
+        frame.setLocationRelativeTo(null);
+        frame.setFocusable(true);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_SPACE){
+                    JOptionPane.showMessageDialog(null,"Miroslav");
+                }
+            }
+        });
+        frame.setVisible(true);
+
+
+
+
     }
 }
